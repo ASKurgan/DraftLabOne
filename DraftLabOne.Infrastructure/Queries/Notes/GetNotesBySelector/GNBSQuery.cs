3c26a24c31a442be16581f08d2a7cb5af7445169
ASKurgan <infopret@yandex.ru>
2025-02-28 18:52:54 +0300

    added GNBSQuery class. Empty for now. Will be filled a little later

---
+++ DraftLabOne.Infrastructure/Queries/Notes/GetNotesBySelector/GNBSQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraftLabOne.Infrastructure.Queries.Notes.GetNotesBySelector
+{
+    public class GNBSQuery
+    {
+    }
+}
