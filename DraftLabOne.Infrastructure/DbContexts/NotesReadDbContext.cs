a4feff1c08f40ce2bc08fdffae7106193048188a
ASKurgan <infopret@yandex.ru>
2025-02-28 18:38:24 +0300

    Add ReadDbContext

--- DraftLabOne.Infrastructure/DbContexts/NotesReadDbContext.cs
+++ DraftLabOne.Infrastructure/DbContexts/NotesReadDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,36 @@ using System.Threading.Tasks;
 
 namespace DraftLabOne.Infrastructure.DbContexts
 {
-    public class NotesReadDbContext
+    public class NotesReadDbContext : DbContext
     {
+        private readonly IConfiguration _configuration;
 
+
+        public NotesReadDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("PostgreSQL");
+
+           // optionsBuilder.UseSnakeCaseNamingConvention();
+
+            optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(NotesReadDbContext).Assembly,
+                type => type.FullName?.Contains("Configurations.Read") ?? false);
+        }
     }
 }
